eb8e9657489ffe2aa8c135964b02974e00e11ac8
rov <valadares.thel@gmail.com>
2016-03-19 17:29:15 -0300

    Adds empty field validation and business rules

--- sovina_ios/ViewController.swift
+++ sovina_ios/ViewController.swift
@@ -18,8 +18,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func message(viewController: UIViewController, message: String) {
+        let alert = UIAlertController(title: "Atention", message: "\(message)", preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "ok", style: .Default) { _ in })
+        viewController.presentViewController(alert, animated: true){}
+    }
 
     @IBAction func touchButtonCompare(sender: UIButton) {
+        
+        
+        
+        if textFieldQuantityProduct01.text!.isEmpty {
+            self .message(self, message: "Product 1 empty field: Quantity")
+        } else if textFieldPriceProduct01.text!.isEmpty {
+            self .message(self, message: "Product 1 empty field: Price")
+        } else if textFieldQuantityProduct02.text!.isEmpty {
+            self .message(self, message: "Product 2 empty field: Quantity")
+        } else if textFieldPriceProduct02.text!.isEmpty {
+            self .message(self, message: "Product 2 empty field: Price")
+        } else {
+            
+            let quantityProduct01 = Double(textFieldQuantityProduct01.text!)!
+            let priceProduct01 = Double(textFieldPriceProduct01.text!)!
+            let quantityProduct02 = Double(textFieldQuantityProduct02.text!)!
+            let priceProduct02 = Double(textFieldPriceProduct02.text!)!
+            let result = (quantityProduct02 * priceProduct01) / quantityProduct01
+            
+            if(priceProduct02 > result) {
+                
+            } else if (priceProduct02 < result) {
+                
+            } else {
+                
+            }
+        }
         
     }
 
